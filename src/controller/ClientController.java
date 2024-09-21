d4ba125414e68a8603d0d7bc96e3fbd456a1b9f2
cajty <fdshjgjgffjhd@gmail.com>
2024-09-21 12:46:38 +0200

    creat that Menu ,that point of enter of  Bati-Cuisine

---
+++ src/controller/ClientController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import model.Client;
+import util.ScannerSingleton;
+
+import java.util.Optional;
+import java.util.Scanner;
+
+public class ClientController {
+    private static final Scanner input = ScannerSingleton.getScanner();
+ public Optional<Client> searchOrAddClient() {
+    System.out.println("searchOrAddClient");
+    return Optional.empty();
+
+
+ }
+}
